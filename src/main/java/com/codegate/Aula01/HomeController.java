5b9c32eb7c2aaf79f08b376948f54676b765436b
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2022-10-21 16:28:33 -0300

    Comit final para a aula-01

--- src/main/java/com/codegate/Aula01/HomeController.java
+++ src/main/java/com/codegate/Aula01/HomeController.java
@@ -1,6 +1,17 @@
 package com.codegate.Aula01;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collections;
+
+@Controller
 public class HomeController {
 
-    
+    @RequestMapping("/")
+    public String homeApp(Model model){
+        model.addAttribute("mensagem","Esta é uma mensagem injetada através do modelo");
+        return "index";
+    }
 }
